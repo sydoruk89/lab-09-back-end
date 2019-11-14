064bc29dd955a8abbb8c3b0d809162ef0560aaff
Roman Sydoruk <sydoruk89@gmail.com>
2019-11-13 20:55:39 -0800

    server.js

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ client.on('error', err => {throw err;});
 app.get('/location', getLocation);
 app.get('/weather', weatherHandler);
 app.get('/trails', getTrails);
+app.get('/movies', getMovies);
 
 
 app.get('*', (request, response) => {
@@ -100,6 +101,22 @@ function getTrails(request, response) {
     });
 }
 
+// Movies handler
+
+function getMovies(request, response){
+  const url = `https://api.themoviedb.org/3/search/movie/?api_key=${process.env.MOVIES_API_KEY}&query=${request.query.data}`;
+  superagent.get(url)
+    .then(data => {
+      const moviesSummaries = data.body.results.map(element => {
+        return new Movies(element);
+      });
+      response.status(200).send(moviesSummaries);
+    })
+    .catch(() => {
+      errorHandler('Something went wrong', request, response);
+    });
+}
+
 
 // error handler
 function errorHandler(error, request, response) {
@@ -132,6 +149,20 @@ function Trails(trail){
   this.conditions = trail.conditionStatus;
   this.condition_date = trail.conditionDate.slice(0, 10);
   this.condition_time = trail.conditionDate.slice(11);
+}
+
+// movies constructor
+
+
+// movies constructor
+function Movies(movie){
+  this.title = movie.title;
+  this.released_on = movie.release_date;
+  this.total_votes = movie.vote_count;
+  this.average_votes = movie.vote_average;
+  this.popularity = movie.popularity;
+  this.image_url = movie.backdrop_path;
+  this.overview = movie.overview;
 }
 // Connect to DB and Start the Web Server
 client.connect()
