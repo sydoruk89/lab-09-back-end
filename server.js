be08122de639959b4013cc5f421032d1ac954819
Roman Sydoruk <sydoruk89@gmail.com>
2019-11-14 16:31:31 -0800

    yelp

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ app.get('/location', getLocation);
 app.get('/weather', weatherHandler);
 app.get('/trails', getTrails);
 app.get('/movies', getMovies);
+app.get('/yelp', getYelp);
 
 
 app.get('*', (request, response) => {
@@ -31,41 +32,38 @@ app.get('*', (request, response) => {
 });
 
 // get info from a user
-Location.fetchLocation = (request, response) => {
-  try {
-    const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${request.query.data}&key=${process.env.GEOCODE_API_KEY}`;
-    superagent.get(url)
-      .then(data => {
-        const geoData = data.body;
-        const location = (new Location(request.query.data, geoData));
-        let SQL = 'INSERT INTO locations (search_query, formatted_query, latitude, longitude) VALUES($1, $2, $3, $4) RETURNING *';
-        let values = [request.query.data, location.formatted_query, location.latitude, location.longitude];
-        return client.query(SQL, values);
-      })
-      .then (results => {
-        response.status(200).send(results.rows[0]);
-      });
-  }
-  catch (error) {
-    //some function or error message
-    errorHandler('So sorry, something went wrong', request, response);
-  }
-};
-
-// get data from database if exists else get from a user
 function getLocation(request, response) {
-  const SQL = `SELECT * FROM locations WHERE search_query='${request.query.data}'`;
+  const SQL = 'SELECT * FROM locations WHERE search_query= $1';
   client.query(SQL)
     .then( result => {
       if (result.rowCount > 0) {
         console.log('Location data from SQL');
         response.status(200).send(result.rows[0]);
-      }
-      else {
-        Location.fetchLocation(request, response);
+      } else {
+        const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${request.query.data}&key=${process.env.GEOCODE_API_KEY}`;
+        superagent.get(url)
+          .then(data => {
+            console.log('From API');
+            if (!data.body.results.length) { throw 'No Data'; }
+            else {
+              const geoData = data.body;
+              const location = (new Location(request.query.data, geoData));
+              let newSQL = 'INSERT INTO locations (search_query, formatted_query, latitude, longitude) VALUES($1, $2, $3, $4) RETURNING *';
+              let values = [location.search_query, location.formatted_query, location.latitude, location.longitude];
+              return client.query(newSQL, values)
+                .then(results => {
+                  response.status(200).send(results.rows[0]);
+                })
+                .catch(() => {
+                  errorHandler('Something went wrong', request, response);
+                });
+            }
+          });
       }
     })
-    .catch( error => errorHandler(error));
+    .catch(() => {
+      errorHandler('Something went wrong', request, response);
+    });
 }
 
 //API routes
@@ -117,6 +115,21 @@ function getMovies(request, response){
     });
 }
 
+// yelp handler
+function getYelp(request, response){
+  const url = `https://api.yelp.com/v3/businesses/search?term=restaurants&latitude=${request.query.data.latitude}&longitude=${request.query.data.longitude}`;
+  superagent.get(url).set('Authorization', `Bearer ${process.env.YELP_API_KEY}`)
+    .then(data => {
+      console.log(data.body.business);
+      const restaurantSummaries = data.body.businesses.slice(0, 20).map(element => {
+        return new Yelp(element);
+      });
+      response.status(200).send(restaurantSummaries);
+    })
+    .catch(() => {
+      errorHandler('Something went wrong', request, response);
+    });
+}
 
 // error handler
 function errorHandler(error, request, response) {
@@ -164,6 +177,16 @@ function Movies(movie){
   this.image_url = movie.backdrop_path;
   this.overview = movie.overview;
 }
+
+// yelp constructor
+function Yelp(business){
+  this.name = business.name;
+  this.image_url = business.image_url;
+  this.price = business.price;
+  this.rating = business.rating;
+  this.url = business.url;
+}
+
 // Connect to DB and Start the Web Server
 client.connect()
   .then( () => {
